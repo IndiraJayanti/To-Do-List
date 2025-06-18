0fc9335bc67b892e2b3b0dd313326c2dda8fcc71
Christian Putri <christianptrii@gmail.com>
2025-06-18 20:32:37 +0800

    Menambahkan widgets search bar ke home screen & integrasi

---
+++ flutter_app/lib/features/home/widgets/search_bar.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class SearchBar extends StatelessWidget {
+  final TextEditingController controller;
+  final Color pastelGreen;
+
+  const SearchBar({
+    super.key,
+    required this.controller,
+    required this.pastelGreen,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Container(
+        decoration: BoxDecoration(
+          color: pastelGreen,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 12),
+        child: TextField(
+          controller: controller,
+          decoration: const InputDecoration(
+            border: InputBorder.none,
+            hintText: 'Search notes...',
+            icon: Icon(Icons.search),
+          ),
+        ),
+      ),
+    );
+  }
+}
