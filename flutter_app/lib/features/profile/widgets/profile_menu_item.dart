7e94af139460420f611cdf1b64fd7fe919da4e8f
DanendraDnr <danendraputra666@gmail.com>
2025-06-18 20:51:31 +0800

    Menambahkan widget menu item di halaman profile

---
+++ flutter_app/lib/features/profile/widgets/profile_menu_item.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class ProfileMenuItem extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final VoidCallback onTap;
+
+  const ProfileMenuItem({
+    super.key,
+    required this.icon,
+    required this.title,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 10),
+        padding: const EdgeInsets.all(18),
+        decoration: BoxDecoration(
+          color: const Color(0xFFCDEAC0),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Row(
+          children: [
+            Icon(icon, color: const Color(0xFF3F6B3F)),
+            const SizedBox(width: 16),
+            Expanded(
+              child: Text(
+                title,
+                style: const TextStyle(fontSize: 16, color: Colors.black87),
+              ),
+            ),
+            const Icon(
+              Icons.arrow_forward_ios,
+              size: 16,
+              color: Colors.black38,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
